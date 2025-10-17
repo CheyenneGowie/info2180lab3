51315c65837c5b0eefb55c1a7db2d849e3837c80
Cheyenne Gowie <yenniegowie6@gmail.com>
2025-10-16 20:53:10 -0500

    Added hover effect for Tic Tac Toe squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,4 +5,29 @@ squares.forEach(square=>{
     square.classList.add("square");
 });
 console.log("Tic Tac Toe board syled successfully!");
+//====Excersise2====
+let currentPlayer= "X";
+let boardState= Array(9).fill(null);
+
+squares.forEach((square,index)=>{
+    square.addEventListener("click",() =>{
+        if(!boardState[index]){
+            boardState[index]= currentPlayer;
+            square.textContent= currentPlayer;
+            square.classList.add(currentPlayer);
+
+            currentPlayer= currentPlayer=== "X" ? "O" : "X";
+        }
+    })
+})
+
+//===Excersise 3===
+squares.forEach(square => {
+    square.addEventListener("mouseenter",() =>{
+        square.classList.add("hover");
+    });
+    square.addEventListener("mouseleave",()=>{
+        square.classList.remove("hover");
+    });
+});
 });
